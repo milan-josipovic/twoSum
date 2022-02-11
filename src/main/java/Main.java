dd0babaf648553075ba8b2ae06c7b50b1389b175
milan-josipovic <milan.josipovic@protonmail.com>
2022-02-11 13:44:00 +0100

    Result

---
+++ src/main/java/Main.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class Main {
+    public static int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if(nums[j] == target - nums[i]) {
+                    return new int[] {i, j};
+                }
+            }
+        }
+        return null;
+    }
+    public static void main(String[] args) {
+        int[] output = twoSum(new int[] {2,7,11,15}, 9);
+        System.out.println(Arrays.toString(output));
+    }
+}
